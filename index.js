2415246c6afc6a9c8fdeac333ec9c31555d263b4
mayankrai001 <mayankrai627@gmail.com>
2025-06-04 09:38:22 +0530

    first push

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:fileName", (req, res) => {
+  fs.readFile(`./files/${req.params.fileName}`, "utf-8", (err, fileData) => {
+    res.render("show", {
+      fileName: req.params.fileName,
+      fileData: fileData,
+    });
+    console.log("fileData", fileData);
+  });
+});
+
+app.post("/create", (req, res) => {
+  console.log("Hello req.body.title ", req.body.title);
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    `${req.body.details}`,
+    () => {
+      res.redirect("/");
+    }
+  );
+});
+
+app.listen(3000);
